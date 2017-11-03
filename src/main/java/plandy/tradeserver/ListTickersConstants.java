5e57e60f7f25b4cc57ed5d028217e5d41627f4be
Paul Landymore <p.landymore@gmail.com>
2017-11-03 20:01:30 +0000

    Working handling for both request types. Still in Main

---
+++ src/main/java/plandy/tradeserver/ListTickersConstants.java
@@ -0,0 +1,23 @@
+package plandy.tradeserver;
+
+public class ListTickersConstants {
+
+    public static final String tickers = "ticker,fullname\n"+
+        "AAPL,Apple Inc.\n"+
+		"BABA,Alibaba Group Holding Limited\n"+
+		"BAC,Bank pf America Corporation\n"+
+		"C,Citigroup Inc.\n"+
+		"F,Ford Motor Co.\n"+
+		"FB,Facebook, Inc.\n"+
+		"GE,General Electric Company\n"+
+		"GM,General Motors Company\n"+
+		"HPQ,HP Inc.\n"+
+		"IBM,International Business Machines Corporation\n"+
+		"JPM,JPMorgan Chase & Co.\n"+
+		"MRO,Marathon Oil Corporation\n"+
+		"MSFT,Microsoft Corporation\n"+
+		"PFE,Pfizer Inc.\n"+
+		"PG,The Procter & Gamble Company\n"+
+		"VZ,Verizon Communications Inc.\n";
+
+}
